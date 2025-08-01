55c0eb1a39d979ee1687549338c75911bb7c57de
VasuSrivastav <vasusrivastav1998@gmail.com>
2025-08-01 17:00:47 +0530

    index done

---
+++ assets/script.js
@@ -0,0 +1,117 @@
+// hero.js
+document.addEventListener('DOMContentLoaded', () => {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false,
+  });
+
+  const heroSwiper = new Swiper('.mySwiper', {
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    effect: 'fade',
+    fadeEffect: { crossFade: true },
+    loop: true,
+    speed: 1000,
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    on: {
+      init: function () {
+        handleSlideAOS(this);
+      },
+      slideChangeTransitionStart: function () {
+        handleSlideAOS(this);
+      },
+    },
+  });
+
+// Swiper Initialization
+const testimonialSwiper = new Swiper(".testimonialSwiper", {
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+// Live typing effect
+let typedStarted = false;
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && !typedStarted) {
+      typedStarted = true;
+      new Typed("#typed-actual", {
+        strings: ["See what our trusted users Say"],
+        typeSpeed: 60,
+        backSpeed: 30,
+        startDelay: 200,
+        showCursor: false,
+      });
+      observer.disconnect();
+    }
+  });
+}, { threshold: 0.5 });
+
+const testimonialSection = document.querySelector("#testimonials-setion");
+if (testimonialSection) {
+  observer.observe(testimonialSection);
+}
+
+
+
+
+
+  function handleSlideAOS(swiper) {
+    swiper.slides.forEach((slide) => {
+      const elements = slide.querySelectorAll('[data-aos]');
+      elements.forEach((el) => el.classList.remove('aos-animate'));
+    });
+    setTimeout(() => {
+      const activeSlide = swiper.slides[swiper.activeIndex];
+      const elements = activeSlide.querySelectorAll('[data-aos]');
+      elements.forEach((el) => el.classList.add('aos-animate'));
+    }, 100);
+  }
+});
+  document.querySelectorAll('.faq-toggle').forEach(button => {
+    button.addEventListener('click', () => {
+      const content = button.nextElementSibling;
+      const arrow = button.querySelector('.faq-arrow');
+
+      content.classList.toggle('hidden');
+      arrow.classList.toggle('rotate-180');
+    });
+  });
